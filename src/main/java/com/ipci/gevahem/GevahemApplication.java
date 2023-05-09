131a92c745926650ddb99f7f0bdb67f10d9b3370
Boye Aurelyne <boyeaurelyne225@gmail.com>
2023-05-09 12:52:23 +0000

    initial commit

---
+++ src/main/java/com/ipci/gevahem/GevahemApplication.java
@@ -0,0 +1,13 @@
+package com.ipci.gevahem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GevahemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GevahemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipci/gevahem/GevahemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipci.gevahem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GevahemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

83699713fa483d85c1f575106fa91a4471190dd7
Boye Aurelyne <boyeaurelyne225@gmail.com>
2023-05-25 14:35:25 +0000

    suppression des entités client et type client

--- src/main/java/com/ipci/gevahem/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ipci.gevahem.repository;
-
-import com.ipci.gevahem.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByEmail(String email);
-}
--- src/main/java/com/ipci/gevahem/repository/TypeClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ipci.gevahem.repository;
-
-import com.ipci.gevahem.entity.TypeClient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeClientRepository extends JpaRepository<TypeClient, Long> {
-    TypeClient findByLibelle(String libelle);
-}

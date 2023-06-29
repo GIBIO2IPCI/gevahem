e092392dc1d36a31e00e88133b99b6b96fc61b7f
Boye Aurelyne <boyeaurelyne225@gmail.com>
2023-06-29 19:16:02 +0000

    modification de la vue de l'entité prelevement

--- src/main/java/com/ipci/gevahem/entity/Prelevement.java
+++ src/main/java/com/ipci/gevahem/entity/Prelevement.java
@@ -1,30 +1,42 @@
 package com.ipci.gevahem.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data @AllArgsConstructor @NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "libelle"))
 @Entity
 public class Prelevement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "ce champ est obligatoire")
+    @Column(unique = true)
     private String libelle;
+    @NotBlank(message = "ce champ est obligatoire")
     private String unite;
     private String cause_conformite;
-    private Float volume;
+    @NotNull(message = "ce champ est obligatoire")
+    private float volume;
+    @NotNull(message = "ce champ est obligatoire")
     @ManyToOne
     private Patient patient;
+    @NotNull(message = "ce champ est obligatoire")
     @ManyToOne
     private Conformite conformite;
+    @NotNull(message = "ce champ est obligatoire")
     @ManyToOne
     private TypePrelevement typePrelevement;
+    @NotNull(message = "ce champ est obligatoire")
     @OneToMany
     private List<Analyse> analyse = new ArrayList<>();
     @ManyToOne
+    @NotNull(message = "ce champ est obligatoire")
     private Glaciere glaciere;
 
 }
